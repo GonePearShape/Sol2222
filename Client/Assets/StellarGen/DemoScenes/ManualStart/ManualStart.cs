3323ce4902b31e00daa8db3b5b94dbfc578b94a6
GonePearShape <pearshape@protonmail.com>
2017-12-28 23:06:33 +0000

    Shaders, Image Effects

---
+++ Client/Assets/StellarGen/DemoScenes/ManualStart/ManualStart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * This script shows how to manually start StellarGen.
+ */
+public class ManualStart : MonoBehaviour
+{
+    void Awake()
+    {
+        //Starts StellarGen.
+        GameObject.FindGameObjectWithTag("StellarGenController").GetComponent<StellarGen.StellarGenController>().Setup();
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 30), ":: StellarGen Demo ::");
+        GUI.Label(new Rect(10, 40, 400, 30), "Press DELETE to stop StellarGen.");
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Delete))
+        {
+            //Stop StellarGen.        
+            GameObject.FindGameObjectWithTag("StellarGenController").GetComponent<StellarGen.StellarGenController>().Terminate();
+        }
+    }
+}
